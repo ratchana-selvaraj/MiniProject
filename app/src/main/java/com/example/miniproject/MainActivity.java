00bc1002d4d7e5e9a77987ae346b7fa5ce76f585
ratch <skrrachana1@gmail.com>
2021-03-27 22:59:12 +0530

    Last updation

--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.miniproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-
+        Integer index;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText etxt1 = (EditText)findViewById(R.id.etext1);
+        Button bt = (Button)findViewById(R.id.button1);
+        Button bt2 = (Button)findViewById(R.id.button3);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etxt1.getText().toString();
+                RadioButton rb = (RadioButton)findViewById(R.id.radioButton1);
+                RadioButton rb1 = (RadioButton)findViewById(R.id.radioButton2);
+                if (rb.isChecked()){
+                    Intent i = new Intent(MainActivity.this,MainActivity3.class);
+                    startActivity(i);
+                }
+                if (rb1.isChecked()) {
+                    Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                    i.putExtra("res", "Hai " + name + " ");
+                    startActivity(i);
+                }
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                   final String [] listitems = new String[]{"Doctor","Patient"};
+                   AlertDialog.Builder mBuilder=new AlertDialog.Builder(MainActivity.this);
+                   mBuilder.setTitle("Select the type of user");//set title of AlertDialog
+                   mBuilder.setSingleChoiceItems(listitems, -1, new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialogInterface, int i) {
+                           if (listitems[i].equals("Doctor")){
+                               Intent in = new Intent(MainActivity.this,regdoc.class);
+                               startActivity(in);
+                           }
+                           else {
+                               Intent in = new Intent(MainActivity.this,MainActivity3.class);
+                               startActivity(in);
+                           }
+                           dialogInterface.dismiss();
+                       }
+                   });
+                   //show Alert Dialog
+                   AlertDialog mDialog=mBuilder.create();
+                   mDialog.show();
+               }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/miniproject/MainActivity2.java
@@ -0,0 +1,81 @@
+package com.example.miniproject;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView tt = (TextView)findViewById(R.id.atxt1);
+        Button bt = (Button)findViewById(R.id.button);
+        Button bt2 = (Button)findViewById(R.id.button2);
+        Button btt = (Button)findViewById(R.id.button1);
+        Button b2t = (Button)findViewById(R.id.button3);
+        Intent intent = getIntent();
+        String st = (String)intent.getSerializableExtra("res");
+        tt.setText(st);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                String url = "https://www.apollohospitals.com/";
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+    bt2.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            PopupMenu popupMenu = new PopupMenu(MainActivity2.this,view);
+            popupMenu.setOnMenuItemClickListener(MainActivity2.this);
+            popupMenu.inflate(R.menu.main_activity2);
+            popupMenu.show();
+        }
+    });
+        btt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                String url = "https://www.apollohospitals.com/";
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        b2t.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity2.this,view);
+                popupMenu.setOnMenuItemClickListener(MainActivity2.this);
+                popupMenu.inflate(R.menu.main_activity2);
+                popupMenu.show();
+            }
+        });
+    }
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (menuItem.getItemId()) {
+            case R.id.msg:
+                BottomModel bottomModel = new BottomModel();
+                bottomModel.show(getSupportFragmentManager(),"ModelFragment");
+                break;
+            case R.id.mail:
+                Toast.makeText(getApplicationContext()," redirecting to mail",Toast.LENGTH_SHORT).show();
+                break;
+        }return false;
+    }
+}
